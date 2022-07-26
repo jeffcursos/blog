64c5decec27c1b8e3b853ad538ff50c5f70f73bb
jeffcursos <jkr.jef@gmail.com>
2022-07-26 23:21:34 +0000

    aula 3

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
+
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask("hello")
 
+posts = [
+    {
+        "title": "O meu Primeiro Post",
+        "body": "Aqui é o texto do Post",
+        "author": "Feulo",
+        "created": datetime(2022,7,25)
+    },
+    {
+        "title": "O meu Segundo Post",
+        "body": "Aqui é o texto do Post",
+        "author": "Danilo",
+        "created": datetime(2022,7,26)
+    },
+]
+
 @app.route("/")
-
-def hello():
-    return render_template('principal.html')
-    
-@app.route("/meucontato")
-
-def meuContato():
-    return render_template('index.html')
+def index():
+    return render_template("index.html", posts=posts)
