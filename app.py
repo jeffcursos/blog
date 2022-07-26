5a9cbb6bee635f7041c7f512edb2b61b9a92710d
jeffcursos <jkr.jef@gmail.com>
2022-07-26 00:31:47 +0000

    Aula2

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask("hello")
+
+@app.route("/")
+
+def hello():
+    return render_template('principal.html')
+    
+@app.route("/meucontato")
+
+def meuContato():
+    return render_template('index.html')
