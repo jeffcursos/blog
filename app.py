12fbb28b64bfb698a87d86d986d8ac77448a38a8
jeffcursos <jkr.jef@gmail.com>
2022-07-29 00:48:52 +0000

    aula 5  criando login logout e salvando db

--- app.py
+++ app.py
@@ -1,42 +1,111 @@
-
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for, request, flash
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user
+from werkzeug.security import check_password_hash, generate_password_hash
+from sqlalchemy.exc import IntegrityError
 
-app = Flask("hello")
+app = Flask("hello") #nome da aplicação
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "pudim" #criptografia
 
 db = SQLAlchemy (app)
+login = LoginManager(app)
 
 class Post(db.Model):
     __tablename__ = 'posts'
-    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(70), nullable=False)
-    body = db.Column(db.String(500), nullable=False)
-    author = db.Column(db.String(20), nullable=False)
-    created = db.Column(db.DateTime, nullable=False, default=datetime.now) 
+    body = db.Column(db.String(500),nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), nullable=False, unique=True, index=True) 
+    email = db.Column(db.String(64), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    posts = db.relationship('Post', backref='author')
+
+    def set_password (self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int (id)) #convert id que é string para int
 
 db.create_all()
     
-
 @app.route("/")
 def index():
     # Busca no Banco os posts
     posts = Post.query.all()
     return render_template("index.html", posts=posts)
 
-@app.route("/login")
-def login():
-    posts = Post.query.all()
-    return render_template("login.html", posts=posts)
-
 #inserir 2 textos no blog
 @app.route("/populate")
 def populate():
-    post1 = Post(title="Post 1", body="texto do post",author="jef")
-    post2 = Post(title="Post 2", body="texto do post2",author="jef")
+    user = User(username='jeff', email="g@g.com", password_hash='a')
+    post1 = Post(title="Post 1", body="Texto do Post", author=user)
+    post2 = Post(title="Post 2", body="Texto do Post 2", author=user)
+    db.session.add(user)
     db.session.add(post1)
     db.session.add(post2)
     db.session.commit()
-    return redirect("/")
+    return redirect(url_for('index'))
+
+#criar usuarios
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if current_user.is_authenticated:  #verifica se tem usuario logado
+        return redirect(url_for('index'))
+#verifica se método de requisição foi POST, GET aparece dados na barra post não
+
+    if request.method == "POST":
+
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+         
+        try: #tenta fazer cadastro
+            new_user = User(username=username, email=email)
+            new_user.set_password(password) #encriptou password
+            db.session.add(new_user) #adicionou ao usuario
+            db.session.commit() #salvou no db
+        except IntegrityError:
+            flash("Usuário o email já existe!")
+        else:
+            return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user is None or not user.check_password(password):
+            flash("Incorrect Username or Password")
+            return redirect(url_for('login'))
+        login_user(user)
+        return redirect(url_for('index'))
+
+    return render_template("login.html")
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+
+
+
